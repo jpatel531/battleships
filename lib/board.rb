6c66036a14995c6937819b3ae682052f0e988926
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-06-16 14:13:33 +0100

    Started databasing grid etc

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require_relative 'app'
 require 'data_mapper'
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/users.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/main.db")
 set :public, Proc.new {File.join(root, '..', "public")}
 enable :sessions
 
@@ -10,31 +10,52 @@ class User
 	include DataMapper::Resource
 	property :id, Serial
 	property :name, Text, required: true
+	property :number, Integer
+end
+
+class PlayTime
+	include DataMapper::Resource
+	property :id, Serial
+	property :name, Text
+	property :game, Object
+	property :player1, Object
+	property :player2, Object
+	property :player1grid, Object
+	property :player2grid, Object
 end
 
 DataMapper.finalize.auto_upgrade! 
 
 get '/' do 
 	erb :index
+	
 end
 
 post '/' do 
 	if User.count == 0
-		@player1 = User.new
-		@player1.name = params[:name]
-		@player1.save
+		@player1 = User.create(:name => params[:name], :number => 1)
+		session[:number] = 1
 		redirect '/wait/'
 	else
-		@player2 = User.new
-		@player2.name = params[:name]
-		@player2.save
-		redirect '/game'
+		@player2 = User.create(:name => params[:name], :number => 2)
+		session[:number] = 2
+		redirect '/success/'
 	end
 end
 
-get '/wait/' do |num|
+get '/wait/' do
 	redirect "/game" if User.count == 2
 	erb :wait
+end
+
+get '/success/' do 
+	play = PlayTime.create(:name => 'Hello', :game => Game.new)
+	@game = play.game
+	play.update(:player1 => @game.player1)
+	play.update(:player2 => @game.player2)
+	play.update(:player1grid => @game.player1_home_grid)
+	play.update(:player2grid => @game.player2_home_grid)
+	redirect '/game'
 end
 
 get '/destroy' do 
@@ -42,33 +63,39 @@ get '/destroy' do
 end
 
 get '/game' do
-	session[:num] = params[:num]
-	session[:game] ||= Game.new
-	@game = session[:game]
-	session[:player] = (params[:num] == 1) ? @game.player1 : @game.player2
-	@player = session[:player]
-	session[:grid] = (params[:num] == 1) ? @game.player1_home_grid : @game.player2_home_grid
-	@grid = session[:grid]
+	play = PlayTime.first(:name =>'Hello')
+	@homegrid = play.player1grid if session[:number] == 1
+	@homegrid = play.player2grid if session[:number] == 2
 	erb :board
 end
 
 get '/game/ship/:ship' do 
 	session[:ship] ||= params[:ship]
-	redirect "/game"
+	redirect '/game'
 end
 
-get '/:num/game/orientation/:orientation' do 
+get '/game/orientation/:orientation' do 
 	session[:orientation] ||= params[:orientation]
 	redirect '/game'
 end
 
 
-get '/:num/game/coordinate/:coordinate' do 
-	redirect "/game" if session[:ship].nil?
-	@game = session[:game]
-	session[:player].place(session[:ship], params[:coordinate], session[:orientation])	
-	session[:grid].update_for(session[:player])
+get '/game/coordinate/:coordinate' do 
+	play = PlayTime.first(:name => 'Hello')
+	redirect '/game' if session[:ship].nil?
+	if session[:number] == 1
+		play.player1.place(session[:ship], params[:coordinate], session[:orientation])	
+	else
+		play.player2.place(session[:ship], params[:coordinate], session[:orientation])
+	end
+	if session[:number] == 1
+		play.game.player1_home_grid.update_for(play.player1)
+		play.update(:player1grid => play.game.player1_home_grid)
+	else
+		play.game.player2_home_grid.update_for(play.player2)
+		play.update(:player2grid => play.game.player2_home_grid)
+	end
 	session[:ship] = nil
 	session[:orientation] = nil
-	redirect "/game"
+	redirect '/game'
 end
